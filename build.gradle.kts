9416cc862bacfb6a6247274a3eee41423d6e1b08
Joonyoung Lee <jy.lee@anypointmedia.com>
2024-11-28 16:04:08 +0900

    Update 1.0.31-rc1
    Change-Id: I9ce76137b9c32ea360ba56dbdbac295e4417e396

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,12 @@
 plugins {
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.android.application) apply false
+    kotlin("android") version "1.9.22" apply false
+    id("com.android.application") version "8.1.3" apply false
+}
+
+allprojects {
+    repositories {
+        maven(url = "https://maven.anypoint.tv/repository/public")
+        mavenCentral()
+        google()
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,3 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        maven(url = "https://maven.anypoint.tv/repository/public")
-        google()
-        mavenCentral()
-    }
-}
-
-rootProject.name = "FlowerOTTApplication"
-include(":app")
+rootProject.name = "flower-sdk-android-sample"
+include(":android-vod")
+include(":android-interstitial-ad")
